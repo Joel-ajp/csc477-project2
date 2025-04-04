fe5ce981b768de5f02a80f1424d007a6b5fd899f
joel-ajp <andrewjoelporter@gmail.com>
2025-04-03 19:07:42 -0500

    puzzle 2 wiring done

--- Assets/Scripts/Puzzle2/CounterButton.cs
+++ Assets/Scripts/Puzzle2/CounterButton.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
-using TMPro; // Only needed if you use TextMeshPro
+using TMPro;
 
 public class CounterButton : MonoBehaviour
 {
     [SerializeField] private TMP_Text counterText;
+    [SerializeField] private TrainSeatCombinationPuzzle puzzleScript; // Reference to the puzzle script
+    [SerializeField] private int counterIndex; // Which counter this is (0, 1, or 2)
+    
     private int counter = 1;
 
     private void Start()
@@ -19,6 +22,28 @@ public class CounterButton : MonoBehaviour
             counter = 1;
         }
         UpdateCounterText();
+        
+        // Notify the puzzle script about the change
+        if (puzzleScript != null)
+        {
+            puzzleScript.UpdateDigit(counterIndex, true); // true means increment
+        }
+    }
+    
+    public void DecreaseCounter()
+    {
+        counter--;
+        if (counter < 1)
+        {
+            counter = 8;
+        }
+        UpdateCounterText();
+        
+        // Notify the puzzle script about the change
+        if (puzzleScript != null)
+        {
+            puzzleScript.UpdateDigit(counterIndex, false); // false means decrement
+        }
     }
 
     private void UpdateCounterText()
@@ -27,5 +52,21 @@ public class CounterButton : MonoBehaviour
         {
             counterText.text = counter.ToString();
         }
+    }
+    
+    // Method to set the counter value directly (useful for initialization)
+    public void SetCounter(int value)
+    {
+        if (value >= 1 && value <= 8)
+        {
+            counter = value;
+            UpdateCounterText();
+        }
+    }
+    
+    // Getter for the current counter value
+    public int GetCounter()
+    {
+        return counter;
     }
 }
