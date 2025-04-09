22ec0f22f28de26826ffc15139cd2d885b200f76
Zoe B <zjb008@latech.edu>
2025-04-08 22:59:24 -0500

    fixed puzzle 1

--- Assets/Scripts/Puzzle 1/Puzzle 1 Key.cs
+++ Assets/Scripts/Puzzle 1/Puzzle 1 Key.cs
@@ -9,6 +9,10 @@ public class Puzzle1Key : MonoBehaviour
     public static int piecesFound;
     //GameObject of lock that will be enabled/disabled
     public static GameObject Lock;
+    //Amimation control
+    [SerializeField] private Animator animController1;
+    [SerializeField] private Animator animController2;
+    [SerializeField] private Animator animController3;
 
     // Start is called before the first frame update
     void Start()
@@ -47,12 +51,18 @@ public class Puzzle1Key : MonoBehaviour
             //Open gate/remove lock
             print("Puzzle 1 solved");
             Lock.SetActive(false);
+            animController1.SetBool("puzzle_1_completed", true);
+            animController2.SetBool("puzzle_1_completed", true);
+            animController3.SetBool("puzzle_1_completed", true);
         }
         else if (piecesFound >= 0 && piecesFound < 3)
         {
             //Close gate/replace lock
             print("Puzzle 1 unsolved: " + piecesFound + " piece registered");
             Lock.SetActive(true);
+            animController1.SetBool("puzzle_1_completed", false);
+            animController2.SetBool("puzzle_1_completed", false);
+            animController3.SetBool("puzzle_1_completed", false);
         }
         else
         {
